385a7f7f2f7c59d0196adf5f5cf80080fef2290e
SUMIT VOHRA <sumitvohra90@gmail.com>
2017-04-09 23:41:41 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -1,5 +1,25 @@
-// KMP String matching ALGO 
-// prefix[i]  represents length of longest proper prefix  which is equal to proper suffix in the range (0,i) ;  
+#include <bits/stdc++.h>
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define foreach(it, ar) for ( typeof(ar.begin()) it = ar.begin(); it != ar.end(); it++ )
+
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define Int long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define bit(n) (1<<(n))
+#define Last(i) ( (i) & (-i) )
+#define sq(x) ((x) * (x))
+#define INF INT_MAX
+#define mp make_pair
+using namespace  std ;
+
+// KMP String matching ALGO
+// prefix[i]  represents length of longest proper prefix  which is equal to proper suffix in the range (0,i) ;
 //Referencr codeforces - Messenger, 344- div - 2  , handle - alpha go
 std::vector<Int> Build( string  s )
   {
@@ -14,11 +34,12 @@ std::vector<Int> Build( string  s )
           if( s[i] == s[j])   ++j ;
           pref[i]  =  j ;
         }
- 
+
       return pref ;
 
 
   }
+
 
 Int KMP ( string  text ,  string pat)
   {
@@ -29,15 +50,15 @@ Int KMP ( string  text ,  string pat)
        if ( m >  n ) return  0 ;  // Corner Case
        Int ans = 0 ;
 
-        if (m == 1) {   // if m == 1 brute force 
+        if (m == 1) {   // if m == 1 brute force
         for (int i = 0; i < n; i++)
             if( pat[0] == text[i] )
                  ans ++;
-                
+
                 return ans;
               }
 
-        if (m == 2) {  // brute force again 
+        if (m == 2) {  // brute force again
             for (int i = 0; i < n - 1; i++)
              if (pat[0] == text[i] && pat[1] == text[i + 1])
                                 ans++;
@@ -53,13 +74,29 @@ Int KMP ( string  text ,  string pat)
          {  while( q > 0 && pat[q] != text[i] ) // If next character does not matches
                    { q = Prefix[q] ; }
 
-             if( patex[q] ==  text[i] )
+             if( pat[q] ==  text[i] )
                  q = q + 1 ;
 
-             if( q == m )
+             if( q == m  )
               { ans += 1 ;
                 q = Prefix[q-1] ;
                }
 
-                   return ans; }
+          }
+
+    return ans ;
+  }
+
+
+ int main ( )
+  {
+
+     string text , pat  ;
+     cout<<"Enter the text " << endl ;
+     cin >> text ;
+     cout<<"Enter the pattern" << endl ;
+     cin >> pat ;
+     cout<<" Number of occurance of pattern in string " <<  endl ;
+     cout<<KMP( text , pat ) << endl ;
+
   }
